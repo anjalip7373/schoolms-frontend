573614dab8213986d61e198a346af4eba52e36f9
anjalip7373 <anjalip@mntech.co.in>
2026-05-28 16:50:12 +0530

    fix api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,10 @@
-// import axios from 'axios';
-
-// const API = axios.create({
-//   baseURL: 'http://localhost:5000/api',
-// });
-
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// export default API;
-
-
-
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ 
+  baseURL: process.env.REACT_APP_API_URL 
+    ? `${process.env.REACT_APP_API_URL}/api` 
+    : 'http://localhost:5000/api' 
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
@@ -37,4 +25,3 @@ API.interceptors.response.use(
 );
 
 export default API;
- 
